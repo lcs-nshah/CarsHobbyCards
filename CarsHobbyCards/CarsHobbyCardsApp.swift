5e9b659f6c02abdf8ee654b42fea8a73d11491d2
Neil Shah <nshah@lcs.on.ca>
2024-12-18 11:36:01 -0500

    Initial Commit

---
+++ CarsHobbyCards/CarsHobbyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsHobbyCardsApp.swift
+//  CarsHobbyCards
+//
+//  Created by Neil Shah on 2024-12-18.
+//
+
+import SwiftUI
+
+@main
+struct CarsHobbyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
