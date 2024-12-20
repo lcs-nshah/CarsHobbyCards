bf1c094369a9399944633fcda58f2b0dacbd1d34
Neil Shah <nshah@lcs.on.ca>
2024-12-20 13:38:33 -0500

    Added a background gradient to the card layout.

--- CarsHobbyCards/Views/CardLayoutView.swift
+++ CarsHobbyCards/Views/CardLayoutView.swift
@@ -11,50 +11,89 @@ struct CardLayoutView: View {
     
     // MARK: Stored Property
     let cardToShow: Card
+    let imgPaint = ImagePaint(image: Image("McLarenP1Car"))
     
     // MARK: Computed Property
     var body: some View {
         ZStack {
+            
+            // Background gradient
+            VStack {
+                Rectangle()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(
+                                stops: [
+                                    .init(color: .black, location: 0),
+                                    .init(
+                                        color: .gray,
+                                        location: 0.33
+                                    )
+                                ]
+                            ),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    )
+                
+                Rectangle()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(
+                                stops: [
+                                    .init(color: .gray, location: 0.8),
+                                    .init(
+                                        color: .black,
+                                        location: 1.1
+                                    )
+                                ]
+                            ),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    )
+            }
+            
+            // Border
             Trapezoid()
-            HStack {
-                GeometryReader { geometry in
-                    VStack {
-                        Triquetra()
-                            .stroke(
-                                Color.yellow,
-                                style: StrokeStyle(
-                                    lineWidth: 3,
-                                    lineCap: .round,
-                                    lineJoin: .round
-                                )
+                .fill(.porsche918SpyderLightGray)
+            
+            // Triquetras
+            GeometryReader { geometry in
+                VStack {
+                    Triquetra()
+                        .stroke(
+                            Color.porsche918SpyderGreen,
+                            style: StrokeStyle(
+                                lineWidth: 3,
+                                lineCap: .round,
+                                lineJoin: .round
                             )
-                            .fill(Color.clear)
-                            .aspectRatio(1.0/1.0, contentMode: .fit)
-                            .position(
-                                x: geometry.size.width/25,
-                                y: geometry.size.height/100
+                        )
+                        .fill(Color.clear)
+                        .aspectRatio(1.0/1.0, contentMode: .fit)
+                        .position(
+                            x: geometry.size.width/25,
+                            y: geometry.size.height/100
+                        )
+                        .padding()
+                    
+                    Triquetra()
+                        .stroke(
+                            Color.porsche918SpyderGreen,
+                            style: StrokeStyle(
+                                lineWidth: 3,
+                                lineCap: .round,
+                                lineJoin: .round
                             )
-                            .padding()
-                        
-                        Spacer()
-                        
-                        Triquetra()
-                            .stroke(
-                                Color.yellow,
-                                style: StrokeStyle(
-                                    lineWidth: 3,
-                                    lineCap: .round,
-                                    lineJoin: .round
-                                )
-                            )
-                            .fill(Color.clear)
-                            .aspectRatio(1.0/1.0, contentMode: .fit)
-                            .position(
-                                x: geometry.size.width/25,
-                                y: geometry.size.height/2.5
-                            )
-                            .padding()
-                    }
+                        )
+                        .fill(Color.clear)
+                        .aspectRatio(1.0/1.0, contentMode: .fit)
+                        .position(
+                            x: geometry.size.width/25,
+                            y: geometry.size.height/2.5
+                        )
+                        .padding()
                 }
             }
             Text(cardToShow.carName)
